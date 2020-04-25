ed30fc259334f5983ba6db4dce7151fdd21fa4cb
William Gadd <student@ucsc.edu>
2020-04-25 16:13:20 -0700

    skeleton for endless runner

---
+++ src/main.js
@@ -0,0 +1,17 @@
+
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
+  }
+
+let game = new Phaser.Game(config);
+
+// define game settings
+game.settings = {
+  
+}
+
+// reserve keyboard vars
+let keyDOWN, keyUP, keyLEFT, keyRIGHT;
